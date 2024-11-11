d155babb37db860b8744fbe61672b37276fd6db6
SaDR4M <mjanloo83@gmail.com>
2024-11-11 16:20:41 +0330

    add resume view

--- authentication/employer/models.py
+++ authentication/employer/models.py
@@ -1,21 +1,21 @@
-from django.db import models
-# local import
-from account.models import User
-from package.models import Package
-# Create your models here.
+        from django.db import models
+        # local import
+        from account.models import User
+        from package.models import Package
+        # Create your models here.
 
-class JobOpportunity(models.Model):
+        class JobOpportunity(models.Model):
 
-    active = models.BooleanField(default=True)
-    title = models.CharField(max_length=100)
-    description = models.TextField(max_length=500)
-    created_at = models.DateTimeField(auto_now_add=True)
-    expire_at = models.DateTimeField()
+            active = models.BooleanField(default=True)
+            title = models.CharField(max_length=100)
+            description = models.TextField(max_length=500)
+            created_at = models.DateTimeField(auto_now_add=True)
+            expire_at = models.DateTimeField()
 
 
-class Employer(models.Model):
+        class Employer(models.Model):
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    company_name = models.CharField(max_length=100)
-    job_opportunity = models.ManyToManyField(JobOpportunity, related_name='job_opportunities')
-    package = models.ManyToManyField(Package , related_name='packages')
+            user = models.ForeignKey(User, on_delete=models.CASCADE)
+            company_name = models.CharField(max_length=100)
+            job_opportunity = models.ManyToManyField(JobOpportunity, related_name='job_opportunities')
+            package = models.ManyToManyField(Package , related_name='packages')
--- authentication/job_seeker/models.py
+++ authentication/job_seeker/models.py
@@ -2,11 +2,27 @@ from django.db import models
 # local import
 from account.models import User
 
+from authentication.employer.models import Employer
+
 
 # Create your models here.
 class JobSeeker(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     resume = models.FileField(upload_to='resumes/')
+
     # json field for the resume creation
     # resumse = models.JSONField(default=dict)
 
+class ResumeView(models.Model) :
+    class ResumeStatus(models.IntegerChoices):
+        SENT = 0 , 'sent'
+        REJECTED = 1 , 'rejected'
+        SEEN = 2 , 'seen'
+        INTERVIEW = 3 , 'interview'
+        ACCEPTED = 4 , 'accepted'
+
+    job_seeker = models.ForeignKey(JobSeeker, on_delete=models.CASCADE)
+    employer = models.ForeignKey(Employer, on_delete=models.CASCADE)
+    view_at = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=ResumeStatus.choices, default=ResumeStatus.SENT)
+    description = models.TextField()

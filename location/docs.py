3f94df4953026283d1ecc3e2b73c1ad8224f015c
SaDRa <mjanloo83@gmail.com>
2025-02-26 06:53:59 +0000

    Update: location doc

--- location/docs.py
+++ location/docs.py
@@ -10,32 +10,6 @@ all_provinces_get_doc = swagger_auto_schema(
             examples={
                 "application/json" :
                         [
-                            {
-                                "id": 1,
-                                "name": "اسکو",
-                                "slug": "اسکو",
-                                "province_id": 1
-                            },
-                            {
-                                "id": 2,
-                                "name": "اهر",
-                                "slug": "اهر",
-                                "province_id": 1
-                            },
-                        ]
-                }
-        )
-    }
-)
-all_cities_get_doc = swagger_auto_schema(
-    operation_description="list of all cities",
-    operation_summary="list of all cities",
-    responses={
-        200 : openapi.Response(
-        description="successful",
-        examples={
-            "application/json" :
-                    [
                         {
                             "id": 1,
                             "name": "آذربایجان شرقی",
@@ -48,10 +22,38 @@ all_cities_get_doc = swagger_auto_schema(
                             "slug": "آذربایجان-غربی",
                             "tel_prefix": "044"
                         },
+                        ]
+                }
+        )
+    },
+    security=[{"Bearer" : []}]
+)
+all_cities_get_doc = swagger_auto_schema(
+    operation_description="list of all cities",
+    operation_summary="list of all cities",
+    responses={
+        200 : openapi.Response(
+        description="successful",
+        examples={
+            "application/json" :
+                    [
+                    {
+                        "id": 1,
+                        "name": "اسکو",
+                        "slug": "اسکو",
+                        "province_id": 1
+                    },
+                    {
+                        "id": 2,
+                        "name": "اهر",
+                        "slug": "اهر",
+                        "province_id": 1
+                    },
                     ]
             }
     )
-        }
+        },
+    security=[{"Bearer" : []}]
 )
 province_cities_get_doc = swagger_auto_schema(
     operation_description="list of a province cities",
@@ -67,18 +69,19 @@ province_cities_get_doc = swagger_auto_schema(
                     [
                         {
                             "id": 1,
-                            "name": "آذربایجان شرقی",
-                            "slug": "آذربایجان-شرقی",
-                            "tel_prefix": "041"
+                            "name": "اسکو",
+                            "slug": "اسکو",
+                            "province_id": 1
                         },
                         {
                             "id": 2,
-                            "name": "آذربایجان غربی",
-                            "slug": "آذربایجان-غربی",
-                            "tel_prefix": "044"
+                            "name": "اهر",
+                            "slug": "اهر",
+                            "province_id": 1
                         },
                     ]
             }
         )
-    }
+    },
+    security=[{"Bearer" : []}]
 )
